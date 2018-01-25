5f3ea535d9ee8b8fa1cc0e238abe97570356d92f
christalvo <christalvo@gmail.com>
2018-01-25 10:12:08 -0800

    Added some more javascript; TODO: show/hide descriptions.

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -10,9 +10,36 @@ $(document).ready(function() {
  */
 function initializePage() {
 	$("#testjs").click(function(e) {
-		$('.jumbotron h1').text("Javascript is connected");
+		$('.jumbotron h1').text("Javascript is has taken control");
+        $("#testjs").text("Please wait...")
+        $("testjs").addClass("active")
+        $("testjs").toggleClass("active")
 	});
 
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
+
+	function projectClick(e) { 
+    // prevent the page from reloading 
+		console.log("Project clicked")
+    e.preventDefault();
+    // In an event handler, $(this) refers to 
+    // the object that triggered the event 
+    $(this).css("background-color", "#7fff00");
+
+		$(".project").click(function(){
+			 $("#description").toggle();
+		})
+
+
+/*
+var containingProject = $(this).closest(".project");
+    var description = $(containingProject).find(".project-description");
+    if (description.length == 0) {
+       $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
+    } else {
+       description.html("<p>Stop clicking on me! You just did it at " + (new Date()) + "</p>");
+   }*/
+}
+	$("a.thumbnail").click(projectClick);
 }
